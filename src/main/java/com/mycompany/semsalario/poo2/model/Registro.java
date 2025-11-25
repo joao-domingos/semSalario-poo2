249272fd19e6905a3a517bb859921907073a59e2
joao-domingos <joaopedro.mdomingos@gmail.com>
2025-11-25 05:14:07 -0300

    classe model/registro reutilizada do projeto excluido

---
+++ src/main/java/com/mycompany/semsalario/poo2/model/Registro.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.semsalario.poo2.model;
+
+/**
+ *
+ * @author jp
+ */
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Registro {
+    private int id;
+    private String name;
+    private String date;
+    private String time;
+    private String type;
+    private float value;
+    private String description;
+
+    public Registro(int id, float value) {
+        this.id = id;
+        this.date = LocalTime.now().toString();
+        this.time = LocalDate.now().toString();
+        this.value = value;
+    }
+
+    public Registro(int id, String name, String date, String time, String type, float value, String description) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.type = type;
+        this.value = value;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate() {
+        this.date = LocalDate.now();
+    }
+
+    public void setTime() {
+        this.time = LocalTime.now();
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+  }
